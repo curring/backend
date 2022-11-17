2dfe3fd0ac6cdbe943e9b665e207520bfd150d99
Juan M Scaparro <juanscaparro@gmail.com>
2022-11-17 10:26:01 -0300

    chore: refactor destination image

--- src/utils/uploads.ts
+++ src/utils/uploads.ts
@@ -5,7 +5,7 @@ import path, { extname } from 'path';
 
 export const upload = multer({
   storage: multer.diskStorage({
-          destination: path.join(__dirname, '../uploads'),
+          // destination: path.join(__dirname, '../uploads'),
           filename: (req, file, callback) => {
             const fileExtension = extname(file.originalname);
             const fileName = file.originalname.split(fileExtension)[0];
